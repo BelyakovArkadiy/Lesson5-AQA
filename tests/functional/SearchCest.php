9084bbc504d065e00bbc3a4d768f4ad36c452468
ArkadiyBelyakov <belyakovarkadiy@gmail.com>
2022-01-26 20:39:38 +0600

    update function checkSearch

--- tests/functional/SearchCest.php
+++ tests/functional/SearchCest.php
@@ -11,10 +11,10 @@ class SearchCest
     public function checkSearch(FunctionalTester $I)
     {
         $I->amOnPage('');
-        $I->seeElement("//*[@href='/muzhchinam/'][@data-id='1']");
-        $I->click("//*[@href='/muzhchinam/'][@data-id='1']");
-        $I->seeElement('.page__header-view .link--border.link');
-        $I->click('.page__header-view .link--border.link');
-        $I->seeNumberOfElements('.col-6',60);
+        $I->seeElement(".main-menu li:nth-child(5)");
+        $I->click(".main-menu li:nth-child(5)");
+//        $I->seeElement('.page__header-view .link--border.link');
+//        $I->click('.page__header-view .link--border.link');
+//        $I->seeNumberOfElements('.col-6',60);
     }
 }
