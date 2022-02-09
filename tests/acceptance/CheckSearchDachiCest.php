d379f0d703e106c2ee1c062df9ac58d46d275fec
ArkadiyBelyakov <belyakovarkadiy@gmail.com>
2022-02-09 15:59:02 +0600

    добавил DataProvider

--- tests/acceptance/CheckSearchDachiCest.php
+++ tests/acceptance/CheckSearchDachiCest.php
@@ -1,42 +1,53 @@
 <?php
 use Page\Acceptance\MainPage;
 use Page\Acceptance\DachiPage;
+use Codeception\Example;
 
 /**
- * Класс для поиска Дачи
+ * Класс для поиска Дачи для покупки
  */
 class CheckSearchDachiCest
+{
+    public function _before(AcceptanceTester $I)
     {
-            public function _before(AcceptanceTester $I)
-            {
-                $I->amOnPage(MainPage::$URL_RELEASE);
-//
+        $I->amOnPage(MainPage::$URL_RELEASE);
         $I->setCookie('tutorialDisabled', 'true');
-
 //        отключаем сторонние скрипты (QA)
         $I->setCookie('remote-resources-disable', '1');
-            }
+    }
 
     /**
      * @param AcceptanceTester $I
+     * @param Example $data
+     * Проверяем поиск Дачи по регионам Казахстана
      * @return void
-     * Проверяем поиск Дачи по городам Казахстана
+     * @dataProvider getDataSearchRegion
      */
-            public function seachDachainKz(AcceptanceTester $I)
-            {
-                $dachiPage = new DachiPage($I);
+    public function searchDachaInKz(AcceptanceTester $I, Example $data)
+    {
+        $dachiPage = new DachiPage($I);
 
-                $I->amOnPage(DachiPage::$URL);
-                $I->waitForElementVisible(DachiPage::$chooseRegion);
-                $dachiPage->clickToChoiceCity();
-                $I->waitForElementClickable(DachiPage::$almaty);
-                $I->click(DachiPage::$almaty);
-                $I->waitForElementVisible(DachiPage::$btnChoice);
-                $I->click(DachiPage::$btnChoice);
-                $I->waitForElementClickable(DachiPage::$btnSeeResult);
-                $dachiPage->clickToSeeResult();
-                $I->canSeeInCurrentUrl(DachiPage::$URL_DACHI_ALMATY);
+        $I->amOnPage(DachiPage::$URL);
+        $I->waitForElementVisible(DachiPage::$chooseRegion);
+        $dachiPage->clickToChoiceRegion();
+        $I->waitForElementVisible(sprintf(DachiPage::$region, $data['region']));
+        $I->click(sprintf(DachiPage::$region, $data['region']));
+        $I->waitForElementVisible(DachiPage::$btnChoice);
+        $I->click(DachiPage::$btnChoice);
+        $I->waitForElementVisible(DachiPage::$btnSeeResult);
+        $dachiPage->clickToSeeResult();
+        $I->canSeeInCurrentUrl($data['url']);
 
-
-            }
     }
+
+    protected function getDataSearchRegion()
+    {
+        return [
+            ['region' => 'almaty', 'url' => 'almaty'],
+            ['region' => 'nur-sultan', 'url' => 'nur-sultan'],
+            ['region' => 'almatinskaja-oblast', 'url' => 'almatinskaja-oblast'],
+            ['region' => 'shymkent', 'url' => 'shymkent'],
+            ['region' => 'karagandinskaja-oblast', 'url' => 'karagandinskaja-oblast']
+        ];
+    }
+}
